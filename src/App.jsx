15f034318d0d1735905a3556d3ffe0418ff82ab8
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-11 19:25:59 +0530

    added map method and get current date and month using now date () method

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-const App = () => {
-  return (<>
-    <div>
+import { useState } from "react";
+import { GiFastBackwardButton } from "react-icons/gi";
+import { GiFastForwardButton } from "react-icons/gi";
 
+
+const App = () => {
+  const WeaDays =["Sun","Mon","Tue","Wed" ,"Thu","Fri" ,"Sat" ]
+  const months=["January","February","March","April","May","June","July","August","September","October","November","December"]
+const [selectedDate,setSelectedDate]=useState(new Date());
+  const daysInMonth=()=>{
+    const daysArray=[];
+
+    const firstDay= new Date (selectedDate.getFullYear(),selectedDate.getMonth(),1);
+    const lastDay= new Date (selectedDate.getFullYear(),selectedDate.getMonth()+1,0);
+    return daysArray ;
+
+  }
+  daysInMonth()
+  return (<>
+    <div className="calender">
+      <div className="header">
+        <button>
+        <GiFastBackwardButton />
+        </button>
+        <select value={selectedDate.getMonth()} >
+          {months.map((month,index)=>(
+            <option key={index} value={index}>{month}
+            </option>                           
+          ))}
+        </select>
+        <select >
+        </select>
+        <button>
+        <GiFastForwardButton />
+        </button>
+      </div>
+      <div className="Day-list">
+        {WeaDays.map((day)=>(
+          <div className="Days" key={day}> {day} </div>
+        ))}
+      </div>
     </div>
 
-  </>);
+  </>
+);
 }
 
 export default App;
